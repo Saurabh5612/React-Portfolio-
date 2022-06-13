442f7800124d02dc86fcf52c6fbcd191e65c7063
unknown <patelsaurabh171@gmail.com>
2022-06-13 15:38:22 -0400

    updateresume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
 				<h1 className="page-header">My Resume</h1>
 			</div>
 			<div className="bottom-spacing">
-				<a href={require("../../assets/files/saurabh patelResume.pdf")} download>
+				<a href={require("../../assets/files/spResume")} download>
 					<h4>Download my Resume</h4>
 				</a>
 			</div>
